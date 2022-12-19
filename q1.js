23f6df9368a846eda95801c3ea7ac354f7993816
leechiuhui <leechiuhui@gmail.com>
2022-12-19 21:11:51 +0800

    original question

---
+++ q1.js
@@ -0,0 +1,18 @@
+/*
+    Task 1
+    - Create a function that would swap the value of x and y using only x and y as variables.
+    - x and y must be numeric.
+    - return -1 if x and y is not numeric.
+    - print the swapped values in the console
+
+    Task 2
+    - invoke the function "swap"
+*/
+
+function swap(x, y){
+    // Task 1: Add code here
+}
+
+// Task 2: Add code here
+
+module.exports = swap;
